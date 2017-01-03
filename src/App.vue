9a2a40d22e221328802ffcf8a6fb2a9c4d6c40a8
reg2005 <cmp08@ya.ru>
2017-01-03 04:51:31 +0300

    Add transition in router, App Refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,9 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-light bg-faded navbar-fixed-top">
-      <div class="container">
-        <button class="navbar-toggler hidden-md-up" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
-        <div class="collapse navbar-toggleable-sm" id="navbarResponsive">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <ul class="nav navbar-nav">
-            <router-link to="/" tag="li" class="nav-item" exact>
-              <a class="nav-link">Главная</a>
-            </router-link>
-            <router-link to="/topics" tag="li" class="nav-item" exact>
-              <a class="nav-link">Создать форму</a>
-            </router-link>
-            <router-link to="/counter" tag="li" class="nav-item" exact>
-              <a class="nav-link">Создать опрос</a>
-            </router-link>
-            <router-link to="/about" tag="li" class="nav-item" exact>
-              <a class="nav-link">Статистика</a>
-            </router-link>
-          </ul>
-          <form class="form-inline float-lg-right">
-            <input class="form-control" type="text" placeholder="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
-        </div>
-      </div>
-    </nav>
-    <router-view class="container"></router-view>
+    <navbar></navbar>
+    <transition name="fade">
+      <router-view class="container"></router-view>
+    </transition>
   </div>
 </template>
 
@@ -35,6 +11,16 @@
   body {
     padding-top: 70px;
     padding-bottom: 30px;
+  }
+
+  .fade-enter-active{
+    transition: opacity .4s ease;
+  }
+  .fade-leave-active {
+    transition: opacity .1s ease;
+  }
+  .fade-enter, .fade-leave-active {
+    opacity: 0
   }
 </style>
 
@@ -63,7 +49,9 @@
       title: 'Default Title',
       // all titles will be injected into this template
       titleTemplate: '%s | My Awesome Webapp'
-
+    },
+    components: {
+      'navbar': require('./Navbar.vue')
     }
   }
 </script>
---
+++ src/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar navbar-light bg-faded navbar-fixed-top">
+    <div class="container">
+      <button class="navbar-toggler hidden-md-up" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+              aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
+      <div class="collapse navbar-toggleable-sm" id="navbarResponsive">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <ul class="nav navbar-nav">
+          <router-link to="/" tag="li" class="nav-item" exact>
+            <a class="nav-link">Главная</a>
+          </router-link>
+          <router-link to="/topics" tag="li" class="nav-item" exact>
+            <a class="nav-link">Создать форму</a>
+          </router-link>
+          <router-link to="/counter" tag="li" class="nav-item" exact>
+            <a class="nav-link">Создать опрос</a>
+          </router-link>
+          <router-link to="/about" tag="li" class="nav-item" exact>
+            <a class="nav-link">Статистика</a>
+          </router-link>
+        </ul>
+        <!--<form class="form-inline float-lg-right">-->
+          <!--<input class="form-control" type="text" placeholder="Search">-->
+          <!--<button class="btn btn-outline-success" type="submit">Search</button>-->
+        <!--</form>-->
+      </div>
+    </div>
+  </nav>
+</template>
